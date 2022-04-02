1cdd6bb1f9d173163e86a650f132600cf00dc614
Sergio Otero <sergiootero536@gmail.com>
2022-04-02 18:28:04 -0500

    agregando las prop a las entities

---
+++ src/product/entity/categories.entity.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { Document } from 'mongoose';
+
+@Schema()
+export class Category extends Document {
+  @Prop()
+  name: string;
+
+  @Prop()
+  image: string;
+}
+
+export const categorySchema = SchemaFactory.createForClass(Category);
