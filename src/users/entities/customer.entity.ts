7f575edfb3edee1e20ab0ecfe96bac99266563d9
Sergio Otero <sergiootero536@gmail.com>
2022-04-03 14:38:18 -0500

    entity y service de customer y user

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,6 +1,20 @@
-export class Customer {
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+
+import { Document } from 'mongoose';
+
+@Schema()
+export class Customer extends Document {
+  @Prop()
   id: number;
+
+  @Prop()
   name: string;
+
+  @Prop()
   lastName: string;
+
+  @Prop()
   phone: string;
 }
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,20 @@
-export class User {
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop()
   id: number;
+
+  @Prop()
   email: string;
+
+  @Prop()
   password: string;
+
+  @Prop()
   role: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
