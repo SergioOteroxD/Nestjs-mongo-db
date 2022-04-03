6a507d2f6a1685070201fdf9c264bf4f0992166c
Sergio Otero <sergiootero536@gmail.com>
2022-04-03 16:29:37 -0500

    CONECCIONA LA BASE DE DATOS

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,21 @@
 import { Module, Global } from '@nestjs/common';
-
-const API_KEY = '12345634';
-const API_KEY_PROD = 'PROD1212121SA';
+import { MongoClient } from 'mongodb';
 
 @Global()
 @Module({
   providers: [
     {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
-  ],
-  exports: ['API_KEY'],
+      provide: 'MONGO',
+      useFactory: async () => {
+        // 👈 Inject w/ useFactory
+        const uri =
+          'mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary';
+        const client = new MongoClient(uri);
+        await client.connect();
+        const database = client.db('platzi-store');
+        return database;
+      },
+    },],
+  exports: ['MONGO'],
 })
 export class DatabaseModule {}
