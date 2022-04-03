83f32edb0c7f4b8d5b2502283a23a29743bdff98
Sergio Otero <sergiootero536@gmail.com>
2022-04-03 18:22:39 -0500

    agregado todos los schemas

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,8 +1,18 @@
 import { User } from './user.entity';
 import { Product } from './../../products/entities/product.entity';
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export class Order {
+@Schema()
+export class Order extends Document {
+  @Prop()
   date: Date;
+
+  @Prop()
   user: User;
+
+  @Prop()
   products: Product[];
 }
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,9 +8,20 @@ import { UsersService } from './services/users.service';
 import { ProductsModule } from '../products/products.module';
 import { OrderService } from './services/order.service';
 import { OrderController } from './controllers/order.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
+import { Order, OrderSchema } from './entities/order.entity';
+import { Customer, CustomerSchema } from './entities/customer.entity';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Order.name, schema: OrderSchema },
+      { name: Customer.name, schema: CustomerSchema },
+    ]),
+  ],
   controllers: [CustomerController, UsersController, OrderController],
   providers: [CustomersService, UsersService, OrderService],
 })
