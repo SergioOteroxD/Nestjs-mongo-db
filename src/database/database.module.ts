767ae052ba66a6335cd3e6ba97f7225de65ef15e
Sergio Otero <sergiootero536@gmail.com>
2022-04-03 16:43:52 -0500

    implementando variables de ambiente

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,32 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { MongoClient } from 'mongodb';
+import config from '../config';
 
 @Global()
 @Module({
   providers: [
     {
       provide: 'MONGO',
-      useFactory: async () => {
+      useFactory: async (configService: ConfigType<typeof config>) => {
         // 👈 Inject w/ useFactory
-        const uri =
-          'mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary';
+        const {
+          connection,
+          dbname,
+          host,
+          password,
+          port,
+          user,
+        } = configService.mongo;
+        const uri = `${connection}://${user}:${password}@${host}:${port}/?authSource=admin&readPreference=primary`;
         const client = new MongoClient(uri);
         await client.connect();
-        const database = client.db('platzi-store');
+        const database = client.db(`${dbname}`);
         return database;
       },
-    },],
+      inject: [config.KEY],
+    },
+  ],
   exports: ['MONGO'],
 })
 export class DatabaseModule {}
