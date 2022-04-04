66efb4bbb17a7d31a0ea1220ca3da7eb485ca283
Sergio Otero <sergiootero536@gmail.com>
2022-04-03 23:20:24 -0500

    User funcionando

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,45 +1,44 @@
 import { Injectable, NotFoundException, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Model } from 'mongoose';
 
 import { User } from '../entities/user.entity';
 import { Order } from '../entities/order.entity';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
 
 import { ProductsService } from './../../products/services/products.service';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(private productsService: ProductsService) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  findAll() {
-    return 'this.users';
+  async findAll() {
+    const user = await this.userModel.find().exec();
+    return user;
   }
 
-  findOne(id: number) {
-    // if (!user) {
-    //   throw new NotFoundException(`User #${id} not found`);
-    // }
-    return 'user';
+  async findOne(id: string) {
+    const user = await this.userModel.findById(id).exec();
+    if (!user) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return user;
   }
 
-  create(data: CreateUserDto) {
-    return 'newUser';
+  async create(data: CreateUserDto) {
+    const user = await new this.userModel(data);
+    return user.save();
   }
 
-  update(id: number, changes: UpdateUserDto) {
-    return 'this.users[index]';
+  async update(id: string, changes: UpdateUserDto) {
+    const user = await this.userModel
+      .findByIdAndUpdate(id, changes, { new: true })
+      .exec();
+    return user;
   }
 
-  remove(id: number) {
+  async remove(id: string) {
+    await this.userModel.findByIdAndDelete(id).exec();
     return true;
   }
-
-  // getOrderByUser(id: number): Order {
-  //   const user = this.findOne(id);
-  //   return {
-  //     date: new Date(),
-  //     user,
-  //     products: this.productsService.findAll(),
-  //   };
-  // }
 }
